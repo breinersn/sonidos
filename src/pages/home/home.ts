dd4057e14bf33e68dd5aab04ac5bb942eecac73f
rootkits <rootkits@localhost.localdomain>
2018-10-30 22:24:51 -0400

    reproduciendo sonidos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
+import { Animal } from './../../interfaces/animal.interface';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ANIMALES } from '../../data/data.animales';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales: Animal[] = [];
+  constructor() {
 
+ this.animales = ANIMALES.slice(0);
   }
+  public reproducir(animal:Animal){
 
+
+    let audio = new Audio();
+    audio.src = animal.audio;
+
+    audio.load();
+    audio.play();
+
+    setTimeout(()=> animal.reproduciendo = false, animal.duracion * 10);
+  }
 }
